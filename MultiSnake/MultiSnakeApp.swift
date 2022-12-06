d27c4dc572e9ac89be6498d7f07d9d70b9e724f9
Frankreed <baoanhtdn@gmail.com>
2022-12-06 15:22:18 -0800

    Initial Commit

---
+++ MultiSnake/MultiSnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiSnakeApp.swift
+//  MultiSnake
+//
+//  Created by Frank Nguyen on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct MultiSnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
